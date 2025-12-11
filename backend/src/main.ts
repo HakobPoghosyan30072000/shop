9102692bdf41ef7c1c39ec2653eb44ee7132aefd
hakob <hakob.p@xsoft.am>
2025-12-11 12:34:35 +0400

    Update backend main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
 
+console.log('barev');
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
